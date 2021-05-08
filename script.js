1a0229b6153354e769af7f17f8dbc23c74a98c38
RuiOkazaki <okazaki200226@gmail.com>
2021-05-08 21:56:51 +0900

    ドラッグ中に描画されるように変更

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
-const canvas = document.getElementById("canvas"),
-  context = canvas.getContext("2d"),
-  clear = document.getElementById("clear");
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const clear = document.getElementById("clear");
+let isDrag = false;
 
 // 描画処理
 function draw(x, y) {
-  context.lineWidth = 1;
+  if (!isDrag) {
+    return;
+  }
+
+  context.lineWidth = 5;
   context.lineTo(x, y);
   context.stroke();
 }
 
-// イベント
+// マウスの処理
+canvas.addEventListener("mousedown", (e) => {
+  context.beginPath();
+  isDrag = true;
+});
+canvas.addEventListener("mouseup", (e) => {
+  context.closePath();
+  isDrag = false;
+});
 canvas.addEventListener("mousemove", (e) => {
   draw(e.layerX, e.layerY);
+});
+
+// モバイルの処理
+canvas.addEventListener("touchstart", (e) => {
+  context.beginPath();
+  isDrag = true;
+});
+canvas.addEventListener("touchend", (e) => {
+  context.closePath();
+  isDrag = false;
 });
 canvas.addEventListener("touchmove", (e) => {
   draw(e.layerX, e.layerY);
 });
+
+// 全消しボタン
 clear.addEventListener("click", () => {
   context.clearRect(0, 0, canvas.width, canvas.height);
 });
